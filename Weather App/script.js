e261e182e4ba2f9c5f02d243e2e7ca152b5859bd
Yunus İNAN <yns.inan1660@gmail.com>
2021-02-12 20:16:30 +0300

    Changes 12.02.2021

--- Weather App/script.js
+++ Weather App/script.js
@@ -1,4 +1,4 @@
-let weather = {
+var weather = {
     "apiKey": "eb28a26b82f2355194c831d9ca69f91a",
     fetchWeather: function (city) {
         fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${this.apiKey}`)
@@ -21,10 +21,23 @@ let weather = {
         document.querySelector('.wind').innerText = `Wind Speed: ${speed}`;
         document.querySelector('.country').innerText = `Country : ${country}`;
 
+        var x = "red";
+        var color = Math.floor(temp);
+        console.log(color);
+        if (color > 0) {
+            document.body.style.backgroundImage = "none";
+            document.body.style.backgroundColor = "#9B2335";
+
+            console.log("asdasdasd");
+        } else {
+            document.body.style.backgroundImage = "none";
+            document.body.style.backgroundColor = "#34568B";
+        }
     },
     search: function () {
         this.fetchWeather(document.querySelector('.search-bar').value);
-    }
+    },
+
 };
 document.querySelector('.search button').addEventListener('click', function () {
     weather.search();
